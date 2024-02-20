61126c2b4e2391dd4b276c0b9646d12c59168260
mint247 <florian.hackl84@gmail.com>
2024-02-20 09:00:58 +0100

    Created classes

--- Eisenhower_CMD/Tasks.cs
+++ Eisenhower_CMD/Tasks.cs
@@ -5,8 +5,9 @@
         public int CreationDate;
         public int DueDate;
         public Users UserAcc;
+        public eImportance Importance;
     }
-    public enum Importance {
+    public enum eImportance {
         TWO, 
         THREE, 
         FIVE,
