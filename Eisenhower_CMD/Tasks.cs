4cbdda5f3bb2dad543692076a54ec0f885961800
mint247 <florian.hackl84@gmail.com>
2024-02-20 08:58:41 +0100

    Created classes

---
+++ Eisenhower_CMD/Tasks.cs
@@ -0,0 +1,16 @@
+﻿namespace Eisenhower_CMD {
+    public class Tasks {
+        public string Name;
+        public string Description;
+        public int CreationDate;
+        public int DueDate;
+        public Users UserAcc;
+    }
+    public enum Importance {
+        TWO, 
+        THREE, 
+        FIVE,
+        SEVEN
+    }
+
+}
--- Eisenhower_CMD/obj/Debug/net8.0/Eisenhower_CMD.AssemblyInfo.cs
+++ Eisenhower_CMD/obj/Debug/net8.0/Eisenhower_CMD.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Eisenhower_CMD")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+63075266e6bc9f784356b9b33d535c0d187fb617")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f71bdb89a070e8067ce3835db33bbc9845cdd85")]
 [assembly: System.Reflection.AssemblyProductAttribute("Eisenhower_CMD")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Eisenhower_CMD")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
